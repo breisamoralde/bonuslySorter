25f82bd0fa47880098b2b86b832cac106af53983
kreisa <breisa.moralde@orangeandbronze.com>
2016-02-03 01:42:18 +0800

    add limit and refactor url

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 (function () {
     var app = angular.module('bonuslySorter', []);
     
-    app.controller('IndexCtrl', function($http) {
+    app.controller('IndexCtrl', function($http, $filter) {
         var vm = this;
         var baseUrl = 'https://bonus.ly/api/v1/';
         
         vm.accessToken = 'b498b87ee3c4511f16947f4d2970e673';
         vm.userId = '540da6baa454046f8a000001';
+        vm.limit = 50;
+        
         vm.getData = getData;
+        
+        vm.values = [
+            'proactivity',
+            'proficiency',
+            'leadership',
+            'bias-for-action',
+            'speed',
+            'frugality'
+        ];
+        
+        vm.responses = {};
         
         function getData() {
             
-            var usersEndpoint = 'users/' + vm.userId + '/bonuses?access_token=' + vm.accessToken;
+            var usersEndpoint = formUrl();
             var fullUrl = baseUrl + usersEndpoint;
+            var request = formGETRequest();
             
+            $http.get(fullUrl, request).then(function(response) {
+                var bonuses = response.data.result;
+                
+                angular.forEach(vm.values, function(value) {
+                    vm.responses[value] = $filter('filter')(bonuses, {value : value}, true);
+                });
+                
+            });  
+        }
+        
+        function formGETRequest() {
             var request = {};
             request.method = 'GET';
             request.headers = {
                 'Content-Type': 'application/json; charset=utf-8'
-            }   
+            }
             
-            $http.get(fullUrl, request).then(function(response) {
-                console.log('response: ', response)        
-            });  
+            return request;
+        }
+        
+        function formUrl() {
+            return 'users/' + vm.userId + '/bonuses?limit=' + vm.limit + '&skip=0&access_token=' + vm.accessToken; 
         }
         
     });
