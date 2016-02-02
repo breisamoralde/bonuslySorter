902623c3574fd7cd6be02d81b514f4cce2129e81
kreisa <breisa.moralde@orangeandbronze.com>
2016-02-03 01:09:03 +0800

    add getRequest

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 (function () {
     var app = angular.module('bonuslySorter', []);
     
-    app.controller('IndexCtrl', function() {
+    app.controller('IndexCtrl', function($http) {
         var vm = this;
-        vm.test = 'hello';
+        var baseUrl = 'https://bonus.ly/api/v1/';
         
-        var accessToken = 'b498b87ee3c4511f16947f4d2970e673';
-        var baseUrl = 'https://bonus.ly/api/v1/'
-        var usersEndpoint = 'users/';
+        vm.accessToken = 'b498b87ee3c4511f16947f4d2970e673';
+        vm.userId = '540da6baa454046f8a000001';
+        vm.getData = getData;
+        
+        function getData() {
+            
+            var usersEndpoint = 'users/' + vm.userId + '/bonuses?access_token=' + vm.accessToken;
+            var fullUrl = baseUrl + usersEndpoint;
+            
+            var request = {};
+            request.method = 'GET';
+            request.headers = {
+                'Content-Type': 'application/json; charset=utf-8'
+            }   
+            
+            $http.get(fullUrl, request).then(function(response) {
+                console.log('response: ', response)        
+            });  
+        }
+        
     });
     
 })();
