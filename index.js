41f6c530e90bfe9c80b8b0fcff717addca21f31f
kreisa <breisa.moralde@orangeandbronze.com>
2016-02-03 02:31:17 +0800

    add table working version

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
     app.controller('IndexCtrl', function($http, $filter) {
         var vm = this;
         var baseUrl = 'https://bonus.ly/api/v1/';
+        var userEndpoint = 'users/';
         
         vm.accessToken = 'b498b87ee3c4511f16947f4d2970e673';
         vm.userId = '540da6baa454046f8a000001';
         vm.limit = 50;
+        vm.displayName = undefined;
         
         vm.getData = getData;
         
@@ -23,19 +25,33 @@
         vm.responses = {};
         
         function getData() {
-            
-            var usersEndpoint = formUrl();
+            getUserInfo().then(function(userData) {
+                var userInfo = userData.data.result;
+                vm.displayName = userInfo.first_name + ' ' + userInfo.last_name;
+                    
+                getBonuses().then(function(response) {
+                    var bonuses = response.data.result;
+                    console.log('reee', response)
+                    angular.forEach(vm.values, function(value) {
+                        vm.responses[value] = $filter('filter')(bonuses, {value : value, receiver: { username: userInfo.username}}, true);
+                    });
+                });  
+            });
+        }
+        
+        function getBonuses() {
+            var usersEndpoint = userEndpoint + vm.userId + '/bonuses?limit=' + vm.limit + '&skip=0&access_token=' + vm.accessToken; 
             var fullUrl = baseUrl + usersEndpoint;
             var request = formGETRequest();
             
-            $http.get(fullUrl, request).then(function(response) {
-                var bonuses = response.data.result;
-                
-                angular.forEach(vm.values, function(value) {
-                    vm.responses[value] = $filter('filter')(bonuses, {value : value}, true);
-                });
-                
-            });  
+            return $http.get(fullUrl, request);
+        }
+         
+        function getUserInfo() {
+            var meEndpoint = baseUrl + userEndpoint + 'me' + '?access_token=' + vm.accessToken;
+            var request = formGETRequest();
+            
+            return $http.get(meEndpoint, request);
         }
         
         function formGETRequest() {
@@ -46,10 +62,6 @@
             }
             
             return request;
-        }
-        
-        function formUrl() {
-            return 'users/' + vm.userId + '/bonuses?limit=' + vm.limit + '&skip=0&access_token=' + vm.accessToken; 
         }
         
     });
